37fc2f6fd1491d96e8f6f3ea0556c55ed903e486
WagnerRR25 <rodrigueswagner09@gmail.com>
2023-09-14 23:00:07 -0300

    feat: First dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
-import Image from 'next/image'
+'use client'
+
+import { FcGoogle } from "react-icons/fc";
+import { BsTwitter, BsFacebook } from "react-icons/bs";
+import { useCallback, useState } from "react";
+
+import Link from "next/link";
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <div>
+    <h1>Home</h1>
+    <Link href="/dashboard">Dashboard</Link>
+  </div>
+
+
+const [password, setPassword] = useState('');
+const [user, setUser] = useState('');
+
+const handleEntrar = useCallback (() => {
+  console.log(password)
+  console.log(user)
+  
+}, [password, user]);
+
+return (
+  <div className="h-screen">
+    <div className="flex flex-1">
+      <aside className="max-w-lg bg-slate-300">
+        <div className="text-zinc-800 ">
+          <h1 className=" mt-24 mb-32 text-2xl flex items-center justify-center">WRR25 Finance</h1>
+          <h2 className=" mb-4 text-lg flex items-center justify-center">Welcome Back!</h2>
+          <h2 className=" mb-4 flex items-center justify-center">Sign in to continue to Best Finance.</h2>
+
+          <h1 className="px-4 font-bold">Username</h1>
+          <input className="mx-4 mb-4" type="text" placeholder="Enter username" value={user} onChange={e => setUser(e.target.value)}/><br></br>
+          <h1 className="px-4 font-bold">Password</h1>
+          <div className="flex items-center justify-center gap-4">
+          <input type="text" placeholder="Enter password" className="mx-4 mb-4"value={password} onChange={e => setPassword(e.target.value)} />
+          <a href="https://www.google.com" className="text-gray-500 dark:text-black font-bold">Forgot password?</a>
+          </div>
+          <b className="px-4 mb-4">Remember me</b>
+          <button className="bg-violet-500 rounded-sm py-4 px-44 mx-4 mb-4 text-base text-center flex-1 flex items-center justify-center hover:bg-violet-700" onClick={handleEntrar}>Log In</button>
+          <b className="px-32 flex items-center justify-center text-center">- Sign in with -</b>
+          <div className="flex items-center justify-center gap-6 mt-4">
+            <a href="https://pt-br.facebook.com/login/device-based/regular/login/" className="h-9 w-9 px-2.5 py-2.5 bg-violet-500 leading-[2.9] rounded-full">
+              <BsFacebook className="text-white" />
+            </a>
+            <a href="https://twitter.com/i/flow/login" className="h-9 w-9 px-2.5 py-2.5 bg-sky-500 leading-[2.9] rounded-full">
+              <BsTwitter className="text-white" />
+            </a>
+            <a href="https://www.google.com" className="h-9 w-9 px-2.5 py-2.5 bg-red-400 leading-[2.9] rounded-full">
+              <FcGoogle className="text-white" />
+            </a>
+          </div>
+          <div className="mt-24 text-center">
+            <p className="text-gray-500 px-16">
+              Don't have an account?
+              <a href="/signup" className="text-violet-500 font-semibold">
+                Signup now
+              </a>
+            </p>
+            <h2 className="mt-44 px-16">©Minia. Crafted with by Themesbrand</h2>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </aside>
+      <main className="flex-1 bg-violet-500 text-zinc-800 p-6">
+        <div className="h-screen bg-cover relative p-5">
+          <div className="absolute inset-0 bg-violet-500/90"></div>
+          <ul className="bg-bubbles absolute top-0 left-0 w-full h-full overflow-hidden animate-square">
+            <li className="h-10 w-10 rounded-3xl bg-white/10 absolute left-[10%]"></li>
+            <li className="h-28 w-28 rounded-3xl bg-white/10 absolute left-[20%]"></li>
+            <li className="h-10 w-10 rounded-3xl bg-white/10 absolute left-[25%]"></li>
+            <li className="h-20 w-20 rounded-3xl bg-white/10 absolute left-[40%]"></li>
+            <li className="h-24 w-24 rounded-3xl bg-white/10 absolute left-[70%]"></li>
+            <li className="h-32 w-32 rounded-3xl bg-white/10 absolute left-[70%]"></li>
+            <li className="h-36 w-36 rounded-3xl bg-white/10 absolute left-[32%]"></li>
+            <li className="h-20 w-20 rounded-3xl bg-white/10 absolute left-[55%]"></li>
+            <li className="h-12 w-12 rounded-3xl bg-white/10 absolute left-[25%]"></li>
+            <li className="h-36 w-36 rounded-3xl bg-white/10 absolute left-[90%]"></li>
+          </ul>
+        </div>
+       
+      </main>
+    </div>
+  </div>
   )
 }
